9b50d8f1f4ee7cb5c86d00e1857a6606bb1a134a
qriz1452 <112246222+qriz1452@users.noreply.github.com>
2023-06-20 10:00:44 +0530

    Update sonarqube_docker.sh

--- sonarqube_docker.sh
+++ sonarqube_docker.sh
@@ -1,4 +1,4 @@
-docker run -d --name sonarqube -p 9000:9000 -p 9092:9092 sonarqube
+docker run -d --name sonarqube -p 9000:9000  sonarqube:lts
 
 #refrence :
 #https://github.com/vikash-kumar01/installation_scripts/blob/master/sonarqube.sh
