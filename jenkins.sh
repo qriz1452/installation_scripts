7bfe97d01e5c56db1c8a8d5e29dfb3d6ff4cae43
qriz1452 <112246222+qriz1452@users.noreply.github.com>
2023-06-20 09:55:34 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -34,3 +34,20 @@ sudo ufw status
 #https://www.digitalocean.com/community/tutorials/how-to-install-jenkins-on-ubuntu-20-04
 #https://github.com/vikash-kumar01/installation_scripts/blob/master/jenkins.sh
 #https://www.jenkins.io/doc/book/installing/linux/#debianubuntu
+
+# ******************   OR    *****************
+
+## Jenkins installation in ubuntu 16.04
+ 
+ - wget https://updates.jenkins-ci.org/download/war/2.162/jenkins.war ( installs 2.162 version, if you want any other version to be installed visit https://updates.jenkins-ci.org/download/war/ download particular version )
+ - java -jar jenkins.war ( default runs on 8080 port ) 
+ - java -jar jenkins.war --httpPort=5000 ( if you want run on any other port use this, in my case its 5000 port ) 
+ - nohup java -jar jenkins.jar & ( to run jenkins process in background )
+ 
+## Jenkins as a service 
+ - wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+ - echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
+ - apt-get update
+ - apt-get install jenkins
+ - systemctl start jenkins
+ - systemctl status jenkins
